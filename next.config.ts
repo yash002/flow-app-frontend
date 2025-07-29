057b12d606123a6bad89033592e6a511f51075da
yash002 <yash.nittrichy@gmail.com>
2025-07-29 13:09:47 +0530

    update backend url

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,8 @@ const nextConfig = {
   transpilePackages: ['@shopify/polaris'],
   env: {
     NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
+    NEXT_PUBLIC_APP_NAME: process.env.NEXT_PUBLIC_APP_NAME,
+    NEXT_PUBLIC_APP_VERSION: process.env.NEXT_PUBLIC_APP_VERSION,
   },
 }
 
